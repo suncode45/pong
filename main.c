043a7d5fc5562dc519600e7da0aeb22f8d6698b5
suncode45 <>
2025-02-12 20:11:51 -0500

    ”Updated”

--- main.c
+++ main.c
@@ -29,6 +29,11 @@
 
 #include "raylib.h"
 
+double rng(void)
+{
+    return (double)rand()/(RAND_MAX+1.0);
+}
+
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
@@ -39,23 +44,30 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
     int ball_radius = 20;
-    int ball_x = 0;
-    int ball_y = 0;
-    int ball_speed = 3;
-    int ball_xv = ball_speed;
-    int ball_yv = ball_speed;
+    double ball_x = screenWidth / 2;
+    double ball_y = screenHeight / 2;
+    double ball_speed = 2.5;
+    double ball_xv = ball_speed;
+    double ball_yv = ball_speed;
 
     Rectangle paddle1;
-    paddle1.x = 33;
+    paddle1.x = 32;
     paddle1.y = 0;
     paddle1.width = 10;
-    paddle1.height = 100;
+    paddle1.height = 125;
     int paddle_speed = 5;
+
+    Rectangle paddle2;
+    paddle2.x = screenWidth - 32 - 10;
+    paddle2.y = 0;
+    paddle2.width = 10;
+    paddle2.height = 125;
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per
     int frameCount = 0;
+    int score = 0;
     //--------------------------------------------------------------------------------------
 
     // Main game loop
@@ -72,22 +84,42 @@ int main(void)
 
             ClearBackground(BLACK);
             char my_string[256];
-            sprintf(&my_string[0], "%d", frameCount);
+            sprintf(&my_string[0], "%d", score);
 
             frameCount++;
 
             ball_x += ball_xv;
             ball_y += ball_yv;
 
+            if (paddle2.y < ball_y - paddle2.height / 2) {
+                paddle2.y += paddle_speed;
+            }
+
+            if (paddle2.y > ball_y - paddle2.height / 2) {
+                paddle2.y -= paddle_speed;
+            }
+
             if (ball_x < ball_radius){
-                ball_xv = ball_speed;
+                //
             }
             if (ball_x < ball_radius+paddle1.x+paddle1.width && ball_x > ball_radius+paddle1.x && abs((ball_y + ball_radius/2) - (paddle1.y + paddle1.height / 2)) < paddle1.height / 2 + ball_radius/2){
+                ball_speed += 0.125;
                 ball_xv = ball_speed;
-                paddle1.height -= 5;
+                if (score % 2 == 0){
+                    paddle1.height -= 1;
+                    paddle1.y += (int)(rng()+0.5);
+                }
+                score += 1;
+                ball_x = ball_radius+paddle1.x+paddle1.width + ball_xv;
             }
-            if (ball_x + ball_radius > screenWidth){
+            if (ball_x + ball_radius > paddle2.x && ball_x + ball_radius < paddle2.x + paddle2.width && abs((ball_y + ball_radius/2) - (paddle2.y + paddle2.height / 2)) < paddle2.height / 2 + ball_radius/2){
+                ball_speed += 0.125;
                 ball_xv = -ball_speed;
+                if (score % 2 == 0){
+                    paddle2.height -= 1;
+                    paddle2.y += (int)(rng()+0.5);
+                }
+                ball_x = paddle2.x - ball_radius - ball_xv;
             }
             if (ball_y < ball_radius){
                 ball_yv = ball_speed;
@@ -103,10 +135,11 @@ int main(void)
                 paddle1.y += paddle_speed;
             }
 
-            DrawRectangleRec(paddle1, RED);
+            DrawRectangleRec(paddle1, (Color) { 65, 175, 249, 255 });
+            DrawRectangleRec(paddle2, (Color) { 253, 0, 0, 255 });
 
             DrawText(my_string, 2, 0, 21, RAYWHITE);
-            DrawCircle(ball_x,ball_y,ball_radius,BLUE);
+            DrawCircle(ball_x,ball_y,ball_radius,GREEN);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
