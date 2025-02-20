bbcf12dc79b0ce97531063c4dc0be482dfb69896
suncode45 <>
2025-02-19 20:21:57 -0500

    updated

--- main.c
+++ main.c
@@ -43,7 +43,7 @@ int main(void)
     //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
-    int ball_radius = 20;
+    double ball_radius = 20;
     double ball_x = screenWidth / 2;
     double ball_y = screenHeight / 2;
     double ball_speed = 2.5;
@@ -102,30 +102,34 @@ int main(void)
             if (ball_x < ball_radius){
                 //
             }
-            if (ball_x < ball_radius+paddle1.x+paddle1.width && ball_x > ball_radius+paddle1.x && abs((ball_y + ball_radius/2) - (paddle1.y + paddle1.height / 2)) < paddle1.height / 2 + ball_radius/2){
+            if (ball_x < ball_radius+paddle1.x+paddle1.width && ball_x > ball_radius+paddle1.x && abs((paddle1.y+paddle1.height/2) - (ball_y)) < (paddle1.height / 2 + ball_radius/2)){
                 ball_speed += 0.125;
                 ball_xv = ball_speed;
                 if (score % 2 == 0){
                     paddle1.height -= 1;
                     paddle1.y += (int)(rng()+0.5);
                 }
+                ball_yv = -1.6 * ((paddle1.y+paddle1.height/2) - (ball_y))/(paddle1.height) * ball_speed;
                 score += 1;
                 ball_x = ball_radius+paddle1.x+paddle1.width + ball_xv;
             }
-            if (ball_x + ball_radius > paddle2.x && ball_x + ball_radius < paddle2.x + paddle2.width && abs((ball_y + ball_radius/2) - (paddle2.y + paddle2.height / 2)) < paddle2.height / 2 + ball_radius/2){
+            if (ball_x + ball_radius > paddle2.x && ball_x + ball_radius < paddle2.x + paddle2.width && abs((paddle2.y+paddle2.height/2) - (ball_y)) < (paddle2.height / 2 + ball_radius/2)){
                 ball_speed += 0.125;
                 ball_xv = -ball_speed;
                 if (score % 2 == 0){
                     paddle2.height -= 1;
                     paddle2.y += (int)(rng()+0.5);
                 }
+                ball_yv = -1.6 * ((paddle2.y+paddle2.height/2) - (ball_y))/(paddle2.height * 1.6) * ball_speed;
                 ball_x = paddle2.x - ball_radius - ball_xv;
             }
             if (ball_y < ball_radius){
-                ball_yv = ball_speed;
+                ball_yv *= -1;
+                ball_y += ball_yv;
             }
             if (ball_y + ball_radius > screenHeight){
-                ball_yv = -ball_speed;
+                ball_yv *= -1;
+                ball_y += ball_yv;
             }
 
             if(IsKeyDown(KEY_UP)){
